e5c4f9572035319ed7c237c7f209649e29a0ab22
Anil Amrute <60308655+anilamrute@users.noreply.github.com>
2026-03-18 11:25:32 +0530

    Refactor Main class to print 'Hello World!'

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,12 +1,7 @@
+package com.example;
 
 public class Main {
-    
     public static void main(String[] args) {
-        System.out.println("MedAccess Core Libs Sandbox - Booting up!");
-    }
-
-    // A simple dummy method so our unit test has something to check
-    public boolean isSystemHealthy() {
-        return true;
+        System.out.println("Hello World!");
     }
 }
